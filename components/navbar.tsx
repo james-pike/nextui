cec81cd0727c659d9af161e3d2773ff008d58baf
James <jamesandrewpike@gmail.com>
2024-03-01 13:21:16 -0500

    Fix Mobile Navbar Menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -18,11 +18,10 @@ import {
 } from "@nextui-org/react";
 import { ChevronDown, Lock, Activity, Flash, Server, TagUser, Scale } from "./Icons1.jsx";
 import { AcmeLogo } from "./AcmeLogo.jsx";
-import { usePathname, useRouter } from "next/navigation.js";
+import { usePathname } from "next/navigation.js";
 
 export default function App() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const path = usePathname();
 
   const menuItems = [
@@ -32,9 +31,6 @@ export default function App() {
     { name: "Portfolio", route: "/portfolio" },
     { name: "Contact", route: "/contact" },
     { name: "Deployments", route: "/deployments" },
-    { name: "My Settings", route: "/settings" },
-    { name: "Team Settings", route: "/team" },
-    { name: "Help & Feedback", route: "/help" },
     { name: "Log Out", route: "/logout" },
   ];
 
@@ -55,6 +51,7 @@ export default function App() {
   return (
     <Navbar
       onMenuOpenChange={setIsMenuOpen}
+      isMenuOpen={isMenuOpen}
       shouldHideOnScroll
       isBordered
       classNames={{
@@ -104,8 +101,6 @@ export default function App() {
             </Link>
           </NavbarItem>
         ))}
-
-      
       </NavbarContent>
 
       <NavbarContent justify="end">
@@ -123,11 +118,10 @@ export default function App() {
       {isMenuOpen && (
         <NavbarMenu>
           {menuItems.map((item, index) => (
-            <NavbarMenuItem key={index} onClick={handleMenuItemClick} isActive={path === item.route}>
+            <NavbarMenuItem key={index} isActive={path === item.route}>
               <Link
                 color={
                   path == item.route ?
-                  
                      "primary"
                     : index === menuItems.length - 1
                     ? "danger"
@@ -136,6 +130,7 @@ export default function App() {
                 className="w-full"
                 href={item.route}
                 size="lg"
+                onClick={handleMenuItemClick}
               >
                 {item.name}
               </Link>
@@ -146,71 +141,3 @@ export default function App() {
     </Navbar>
   );
 }
-
-
-
-
-
-
-
-
-
-  {/* <Dropdown>
-          <NavbarItem>
-            <DropdownTrigger className="hidden lg:flex">
-              <Button
-                disableRipple
-                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                endContent={icons.chevron}
-                radius="sm"
-                variant="light"
-              >
-                Services
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-              aria-label="ACME features"
-              className="w-[340px]"
-              itemClasses={{
-                base: "gap-4",
-              }}
-            >
-              <DropdownItem
-                key="autoscaling"
-                description="ACME scales apps to meet user demand, automagically, based on load."
-                startContent={icons.scale}
-                onClick={handleServices}
-              >
-                Autoscaling
-              </DropdownItem>
-              <DropdownItem
-                key="usage_metrics"
-                description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
-                startContent={icons.activity}
-              >
-                Usage Metrics
-              </DropdownItem>
-              <DropdownItem
-                key="production_ready"
-                description="ACME runs on ACME, join us and others serving requests at web scale."
-                startContent={icons.flash}
-              >
-                Production Ready
-              </DropdownItem>
-              <DropdownItem
-                key="99_uptime"
-                description="Applications stay on the grid with high availability and high uptime guarantees."
-                startContent={icons.server}
-              >
-                +99% Uptime
-              </DropdownItem>
-              <DropdownItem
-                key="supreme_support"
-                description="Overcome any challenge with a supporting team ready to respond."
-                startContent={icons.user}
-              >
-                +Supreme Support
-              </DropdownItem>
-            </DropdownMenu>
-          </NavbarItem>
-        </Dropdown> */}
