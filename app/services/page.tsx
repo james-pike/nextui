b6187e0c816b7e27e8ebabf0f6108d5a0617861b
James <jamesandrewpike@gmail.com>
2024-03-02 12:01:57 -0500

    Adjusted Layout Width

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -46,7 +46,7 @@ export default function ServicesPage() {
         <div className="grid items-center gap-8 mb-8 lg:mb-16 lg:gap-12 lg:grid-cols-12">
             <div className="col-span-6 text-center sm:mb-6 lg:text-left lg:mb-0">
                 <a href="#" className="inline-flex items-center justify-between px-1 py-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-                    <span className="px-3 py-1 mr-3 text-xs text-white rounded-full bg-primary-600">New</span> <span className="text-sm font-medium">Flowbite is out! See what's new</span> 
+                    <span className="px-3 py-1 mr-3 text-xs text-white rounded-full bg-primary-600">New</span> <span className="text-sm font-medium">Flowbite is out! See whats new</span> 
                     <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                 </a>
                 <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl xl:text-6xl dark:text-white">We invest in the world’s potential</h1>
@@ -72,21 +72,21 @@ export default function ServicesPage() {
                 <svg className="w-6 h-6 mr-3 text-primary-600 dark:text-primary-500 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z" clip-rule="evenodd"></path></svg>
                 <div>
                     <h3 className="mb-1 text-lg font-semibold leading-tight text-gray-900 dark:text-white">Customizable Categories</h3>
-                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you don't want to share with the world in private.</p>
+                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you dont want to share with the world in private.</p>
                 </div>
             </div>
             <div className="flex justify-center">
                 <svg className="w-6 h-6 mr-3 text-primary-600 dark:text-primary-500 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg>
                 <div>
                     <h3 className="mb-1 text-lg font-semibold leading-tight text-gray-900 dark:text-white">Private repos</h3>
-                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you don't want to share with the world in private.</p>
+                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you dont want to share with the world in private.</p>
                 </div>
             </div>
             <div className="flex justify-center">
                 <svg className="w-6 h-6 mr-3 text-primary-600 dark:text-primary-500 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11 4a1 1 0 10-2 0v4a1 1 0 102 0V7zm-3 1a1 1 0 10-2 0v3a1 1 0 102 0V8zM8 9a1 1 0 00-2 0v2a1 1 0 102 0V9z" clip-rule="evenodd"></path></svg>
                 <div>
                     <h3 className="mb-1 text-lg font-semibold leading-tight text-gray-900 dark:text-white">Tracking Saving Rate</h3>
-                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you don't want to share with the world in private.</p>
+                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you dont want to share with the world in private.</p>
                 </div>
             </div>
         </div>
@@ -101,10 +101,10 @@ export default function ServicesPage() {
         <div className="grid items-center gap-8 mb-8 lg:mb-16 lg:gap-12 lg:grid-cols-12">
             <div className="col-span-6 text-center sm:mb-6 lg:text-left lg:mb-0">
                 <a href="#" className="inline-flex items-center justify-between px-1 py-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-                    <span className="px-3 py-1 mr-3 text-xs text-white rounded-full bg-primary-600">New</span> <span className="text-sm font-medium">Flowbite is out! See what's new</span> 
+                    <span className="px-3 py-1 mr-3 text-xs text-white rounded-full bg-primary-600">New</span> <span className="text-sm font-medium">Flowbite is out! See whats new</span> 
                     <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                 </a>
-                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl xl:text-6xl dark:text-white">We invest in the world’s potential</h1>
+                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl xl:text-6xl dark:text-white">We invest in the worlds potential</h1>
                 <p className="max-w-xl mx-auto mb-6 font-light text-gray-500 lg:mx-0 xl:mb-8 md:text-lg xl:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where innovation can unlock long-term value and drive economic growth.</p>
                 <form className="max-w-lg mx-auto lg:ml-0" action="#">
                     <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
@@ -127,14 +127,14 @@ export default function ServicesPage() {
                 <svg className="w-6 h-6 mr-3 text-primary-600 dark:text-primary-500 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z" clip-rule="evenodd"></path></svg>
                 <div>
                     <h3 className="mb-1 text-lg font-semibold leading-tight text-gray-900 dark:text-white">Customizable Categories</h3>
-                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you don't want to share with the world in private.</p>
+                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you dont want to share with the world in private.</p>
                 </div>
             </div>
             <div className="flex justify-center">
                 <svg className="w-6 h-6 mr-3 text-primary-600 dark:text-primary-500 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg>
                 <div>
                     <h3 className="mb-1 text-lg font-semibold leading-tight text-gray-900 dark:text-white">Private repos</h3>
-                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you don't want to share with the world in private.</p>
+                    <p className="font-light text-gray-500 dark:text-gray-400">Host code that you dont want to share with the world in private.</p>
                 </div>
             </div>
             <div className="flex justify-center">
