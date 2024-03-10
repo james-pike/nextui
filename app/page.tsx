bc8cd782045b4b06cafcf819fb07f0e49a1d8c9e
James <jamesandrewpike@gmail.com>
2024-03-10 18:21:43 -0400

    message

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { TypewriterEffectSmooth } from "@/components/typewriter";
+import { Button } from "@nextui-org/button";
 import { Typewriter } from 'react-simple-typewriter'
 
 
@@ -41,7 +42,7 @@ export default function Home() {
 	return (
 		<div>
 <section>
-		<div className="h-[25rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+		<div className="h-[20rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
 			{/* Radial gradient for the container to give a faded look */}
 			<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
@@ -65,8 +66,15 @@ export default function Home() {
 						/>
 					</span>
 				</h1>
+				{/* <div className="flex justify-center space-x-4">
+  <Button size="lg">
+    Get Started
+  </Button>  
 
-			
+  <Button size="lg">
+    Learn More
+  </Button>  
+</div> */}
 
 			</div>
 
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,6 +2,7 @@
 
 import { ImagesSlider } from "@/components/heroslider";
 import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
 
 const images = [
     "https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -11,49 +12,64 @@ const images = [
 
 
 export default function ServicesPage() {
+
+  const handleType = (count: number) => {
+		// access word count number
+		console.log(count)}
+	  
+	
+	  const handleDone = () => {
+		console.log(`Done after 5 loops!`)
+	  }
+
 	return (
+
+    
 
        
 <div>
-{/* <ImagesSlider className="h-[20rem]" images={images}>
-      <motion.div
-        initial={{
-          opacity: 0,
-          y: -80,
-        }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          duration: 0.6,
-        }}
-        className="z-50 flex flex-col justify-center items-center"
-      >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
-          The hero slider <br /> nobody asked for
-        </motion.p>
-        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-          <span>Join now →</span>
-          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-        </button>
-      </motion.div>
-    </ImagesSlider> */}
-{/* <div className="flex flex-col items-center justify-center h-[40rem]  ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-        The road to freedom starts from here
-      </p>
-      <TypewriterEffectSmooth words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-          Join now
-        </button>
-        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-          Signup
-        </button>
-      </div>
-    </div> */}
-    
+
+<section>
+		<div className="h-[20rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+			{/* Radial gradient for the container to give a faded look */}
+			<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+
+			<div className="text-center">
+				<h1 className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8" style={{ paddingTop: '5rem', margin: 'auto 0', fontWeight: 'normal', textAlign: 'center' }}>
+					Our Services{' '}
+					<br></br>
+					<span className="text-primary w-full" style={{ fontWeight: 'bold' }}>
+						{/* Style will be inherited from the parent element */}
+						{/* <Typewriter
+							words={['Web Design', 'Application Development', 'Digital Marketing', 'Branding']}
+							loop={10}
+							cursor
+							cursorStyle='_'
+							typeSpeed={100}
+							deleteSpeed={50}
+							delaySpeed={1000}
+							onLoopDone={handleDone}
+							onType={handleType}
+						/> */}
+					</span>
+				</h1>
+				{/* <div className="flex justify-center space-x-4">
+  <Button size="lg">
+    Get Started
+  </Button>  
+
+  <Button size="lg">
+    Learn More
+  </Button>  
+</div> */}
+
+			</div>
+
+
+		</div>
+
+		</section>
     
 
 <section className="bg-white dark:bg-gray-900">
