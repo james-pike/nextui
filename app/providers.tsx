3b150741cac6f24da4602316c1dcf50a60ac6eb6
James <jamesandrewpike@gmail.com>
2024-03-09 22:53:12 -0500

    Add background

--- app/providers.tsx
+++ app/providers.tsx
@@ -16,16 +16,10 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <Auth0Provider
-    domain="dev-g3fxhe78xxn2i0nq.us.auth0.com"
-    clientId="ZXUvt8kixw3UfSrmngCPxiyTUT84JYIJ"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
+ 
       <NextUIProvider navigate={router.push}>
         <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
       </NextUIProvider>
-    </Auth0Provider>
+
   );
 }
